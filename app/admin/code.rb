5553188353c8af4e9281cb7fbbce28353d72983f
Matt Boldt <me@mattboldt.com>
2013-10-16 00:16:03 -0500

    added admin form partial. edited admin code model. renamed admin initializer.

--- app/admin/code.rb
+++ app/admin/code.rb
@@ -3,13 +3,14 @@ ActiveAdmin.register Code do
         @code = Code.find(params[:id])
 		redirect_to_good_slug(@code) and return if bad_slug?(@code)
       end
-      index do
-	    column :name
-	    column "Release Date", :created_at
-	    column :slug
-	    column :tag_list
-	    default_actions
-	  end
+   #    index do
+	  #   column :name
+	  #   column "Release Date", :created_at
+	  #   column :slug
+	  #   column :tag_list
+	  #   default_actions
+	  # end
+	index :partial => "index"
 	  show do
 	  	attributes_table do
 	  	row :name
@@ -19,16 +20,17 @@ ActiveAdmin.register Code do
 	    row :body
 		end
 	end
-	form do |f|
-		f.inputs "Details", :multipart => true do
-			f.input :name
-			f.input :slug
-			f.input :preview
-			f.input :body
-			f.input :tag_list
-		end
-		f.actions
-	end
+	# form do |f|
+	# 	f.inputs "Details", :multipart => true do
+	# 		f.input :name
+	# 		f.input :slug
+	# 		f.input :preview
+	# 		f.input :body
+	# 		f.input :tag_list
+	# 	end
+	# 	f.actions
+	# end
+	form :partial => "form"
   controller do
     def permitted_params
       params.permit!
