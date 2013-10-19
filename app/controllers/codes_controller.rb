b528894c7c391a4fb56c5b1c83d86d2eaa3e6fb5
Matt Boldt <me@mattboldt.com>
2013-10-19 02:10:04 -0500

    Added codes/tags controller, with route. Added meta tag info on code snippets & tools

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,27 +1,15 @@
 class CodesController < ApplicationController
+
   def index
-    @tags = Code.tag_counts_on(:tags).join(", ").split(", ").sort!
-
-      if params[:tag]
-        sorted_tags = params[:tag].split("/").sort_by!{ |m| m.downcase }.uniq.join("/")
-
-          if params[:tag] != sorted_tags
-            redirect_to code_tags_path+sorted_tags, status: 301
-          else
-            @codes = Code.tagged_with(params[:tag].split("/"))
-          end
-
-        @params = params[:tag].split("/")
-        @tags = @tags-@params
-
-      else
-        @codes = Code.find(:all, :order => "updated_at DESC")
-      end
+      @codes = Code.find(:all, :order => "updated_at DESC")
+      @tags = Code.tag_counts_on(:tags).join(", ").split(", ").sort!
   end
+
   def show
     @code = Code.find(params[:id])
     redirect_to_good_slug(@code) and return if bad_slug?(@code)
   end
+
   def download
     @code = Code.find(params[:id])
     # Rails.logger.info params.inspect
@@ -30,20 +18,5 @@ class CodesController < ApplicationController
       :type => 'text',
       :disposition => "attachment; filename=code-#{@code.created_at.to_formatted_s(:db)}.#{@code.filetype}"
   end
-  # def new
-  #   @category = Category.find_by_url(params[:category_id])
-  #   @codes = @category.codes.build
-  # end
-  # def create
-  #   @code = Code.new(params[:code])
-  #   respond_to do |format|
-  #     if @code.save
-  #       format.html { puts "saved" }
-  #     else
-  #       format.html { puts "error" }
-  #     end
-  #   end
-  # end
-  # def edit
-  # end
+
 end
