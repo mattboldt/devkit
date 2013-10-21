d7e5e0f4d0cd219f445d487f4f535e8a2f119847
Matt Boldt <me@mattboldt.com>
2013-10-20 21:50:49 -0500

    Adding bookmarklet search feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,7 @@ class SearchController < ApplicationController
 	after_filter :set_access_control_headers
 
 	def set_access_control_headers
-		if header['X-Devkit-Bookmark-Search'] == "4Jo8IzIO6JOLhLznh5dBmKNOzbF316W5QBJCpgaU"
-			headers['Access-Control-Allow-Origin'] = '*'
-			headers['Access-Control-Request-Method'] = 'GET'
-		end
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Request-Method'] = 'GET'
 	end
 end
