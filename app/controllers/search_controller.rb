8f6ec1b150a1964805754014039b3a10ce347a22
Matt Boldt <me@mattboldt.com>
2013-10-20 22:01:16 -0500

    added headers for iframe code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
-	after_filter :set_access_control_headers
+	after_filter :set_headers
 
-	def set_access_control_headers
-		headers['Access-Control-Allow-Origin'] = '*'
-		headers['Access-Control-Request-Method'] = 'GET'
+	def set_headers
+		# headers['Access-Control-Allow-Origin'] = '*'
+		# headers['Access-Control-Request-Method'] = 'GET'
+		headers['X-Frame-Options'] = "ALLOWALL"
 	end
 end
