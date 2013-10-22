edacf6e8b70a7c421a92ad6fa1b19f39c8fdca5f
Matt Boldt <me@mattboldt.com>
2013-10-21 22:14:12 -0500

    style changes on code show view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
 	after_filter :set_headers
+	def index
+		if params[:q].empty?
+			@result = "You didn't search for anything"
+		end
+	end
 
 	def set_headers
 		# headers['Access-Control-Allow-Origin'] = '*'
