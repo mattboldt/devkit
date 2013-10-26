46386de74b587970ef2635ca29169d0fc26753e3
Matt Boldt <me@mattboldt.com>
2013-10-26 04:56:08 -0500

    adding custom routing

---
+++ app/controllers/admin_users_controller.rb
@@ -0,0 +1,9 @@
+class AdminUsersController < ApplicationController
+  def index
+    if admin_user_signed_in?
+      @codes = Code.all
+    else
+      redirect_to new_admin_user_session_path
+    end
+  end
+end
