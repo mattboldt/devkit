1a86378b826b0ea4d752ad81fb406c5aa408ed7f
Matt Boldt <me@mattboldt.com>
2013-10-27 16:13:50 -0500

    renamed admin
    Signed-off-by: Matt Boldt <me@mattboldt.com>

---
+++ app/admin/code.rb
@@ -0,0 +1,52 @@
+ActiveAdmin.register Code do
+  before_filter :only => [:show, :edit, :update, :destroy, :preview] do
+        @code = Code.find(params[:id])
+    redirect_to_good_slug(@code) and return if bad_slug?(@code)
+      end
+      index do
+      column :name
+      column "Release Date", :created_at
+      column :tag_list
+      default_actions
+    end
+  # form do |f|
+  #   f.inputs "Details", :multipart => true do
+  #     f.input :name
+  #     f.input :slug
+  #     f.input :preview
+  #     f.input :body
+  #     f.input :tag_list
+  #   end
+  #   f.actions
+  # end
+  form :partial => "form"
+
+
+  #strong params
+  controller do
+
+    # custom controller action
+    def preview
+      require 'redcarpet'
+      preview = params[:code][:body_input]
+      # renderer = Redcarpet::Render::HTML.new(:filter_html => true, :hard_wrap => true, :autolink => true)
+      renderer = PygmentizeHTML
+      extensions = {:autolink => true, :hard_wrap => true, :space_after_headers => true, :highlight => true, :tables => true, :fenced_code_blocks => true, :gh_blockcode => true}
+      redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+      @preview = redcarpet.render preview
+      # respond_to do |format|
+      #   format.json { render @preview }
+      #   format.html { render @preview }
+      #   format.js { render @preview }
+      # end
+      respond_to do |format|
+        format.html { render :text => @preview }
+      end
+    end
+
+
+    def permitted_params
+      params.permit!
+    end
+  end
+end
