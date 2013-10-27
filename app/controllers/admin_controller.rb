13feb4625275ffd72d5042073b8f9bfb18efec2b
Matt Boldt <me@mattboldt.com>
2013-10-27 15:00:12 -0500

    removed excess JS files. Created views and partials for editing code snippets. Got admin user auth working.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
   layout "admin"
-  before_filter :get_models#, :authenticate_user!
+  before_filter :get_models, :authenticate_user!
   helper_method :sort_column, :sort_direction
 
   def get_models
