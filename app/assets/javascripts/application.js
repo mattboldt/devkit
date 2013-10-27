00566728579ab0217ab2b5d3509a8ee26e0e29d8
Matt Boldt <me@mattboldt.com>
2013-10-27 15:00:12 -0500

    removed excess JS files. Created views and partials for editing code snippets. Got admin user auth working.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require main
 
 jQuery.ajaxSetup({
    'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/html")}
