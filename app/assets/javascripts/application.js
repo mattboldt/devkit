8605e970b09a40fbbf7120a80c6095934d418bc9
Matt Boldt <me@mattboldt.com>
2013-11-02 17:19:47 -0500

    added favicon and replaced the SVG in nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require main
+//= require analytics
 
 jQuery.ajaxSetup({
    'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/html")}
